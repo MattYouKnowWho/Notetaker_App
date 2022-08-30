2b9da82d7f68b7a1006f022e8b565c9cf7d91a3a
Matt Dunston <matt.cdunston21@gmail.com>
2022-08-29 22:56:32 -0700

    changes to json file

--- Develop/routes/viewRoutes.js
+++ Develop/routes/viewRoutes.js
@@ -2,11 +2,11 @@ const viewRoutes = require('express').Router()
 const path = require('path')
 
 viewRoutes.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'))
+    res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
 viewRoutes.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'))
+    res.sendFile(path.join(__dirname, './public/notes.html'))
 })
 
 module.exports = viewRoutes;
---
+++ Develop/server.js
@@ -0,0 +1,22 @@
+// Main server file that is run. All respective required() routes are in /routes folder.
+const express = require('express')
+
+const app = express()
+const PORT = process.env.PORT || 3001;
+
+const htmlRoutes = require('./routes/viewRoutes')
+const api = require('./routes/api/index.js')
+
+const middleware = [
+    express.static('public'),
+    express.urlencoded({ extended: true }),
+    express.json()
+]
+app.use(middleware)
+
+// All routes
+app.use(htmlRoutes)
+app.use('/api', api);
+
+
+app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT})`))
