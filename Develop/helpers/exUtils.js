02044cf82ef42a240f2a28a9e94211dc84e7bd6a
Matt Dunston <matt.cdunston21@gmail.com>
2022-08-29 22:17:55 -0700

    added js helpers

---
+++ Develop/helpers/exUtils.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const util = require('util');
+
+// Promise version of fs.readFile
+const readFromFile = util.promisify(fs.readFile);
+/**
+ *  Function to write data to the JSON file 
+ *  @param {string} destination write to file
+ *  @param {object} content what you want to write to file 
+ *  @returns {void} nada
+ */
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+/**
+ *  Function to read data and append files
+ *  @param {object} content To append file
+ *  @param {string} file save file
+ *  @returns {void} nada
+ */
+const readAndAppend = (content, file) => {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
+
+module.exports = { readFromFile, writeToFile, readAndAppend };
